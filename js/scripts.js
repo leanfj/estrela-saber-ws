bce9355df9062dbf1bb6dc177c697ddf106b5569
leanfj <leanfj@hotmail.com>
2017-05-28 15:40:50 -0300

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,23 @@
-// Get the modal
-var modal = document.getElementById('myModal');
+// ELEMENTO MODAL
+var modal = document.getElementById('meuModal');
 
-// Get the button that opens the modal
+// BOTÃO DE ENVIO QUE VAI CHAMAR O MODAL
 var btn = document.getElementById("btn_envia");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// BOTAO PARA FECHAR O MODAL
+var span = document.getElementsByClassName("fecha")[0];
 
-// When the user clicks on the button, open the modal
+// FUNCTION PARA VERIFICAR O EVENTO DE CLICK NO BOTAO
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
+// FUNCTION PARA VERIFICAR O CLICKE NO BOTAO X PARA FECHAR O MODAL
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
+// FUNCTION PARA VERIFICA SE FOI CLICADO FORA DO MODAL PARA FECHALO
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
